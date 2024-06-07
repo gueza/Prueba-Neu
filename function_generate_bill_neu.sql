d31f27dd0c5732d8980afdaeb353c09eb369a1b1
stephania28 <99409852+stephania28@users.noreply.github.com>
2024-06-07 00:14:04 -0500

    Arreglos finales de la funcion sql

--- function_generate_bill_neu.sql
+++ function_generate_bill_neu.sql
@@ -1,27 +1,83 @@
-CREATE OR REPLACE FUNCTION calculate_bill( IN service_id INT, OUT EA FLOAT, OUT EC FLOAT) AS $$
+CREATE OR REPLACE FUNCTION calculate_bill( 
+	IN service_id INT, 
+	OUT EA FLOAT, 
+	OUT EC FLOAT,
+	OUT EE1 FLOAT,
+	OUT EE2 FLOAT) AS $$
 DECLARE
     total_consumption FLOAT;
     total_injection FLOAT;
+	voltage_level_number INT;
     tariff_CU FLOAT;
+	tariff_EE2 FLOAT;
     tariff_C FLOAT;
-    excess_consumption FLOAT;
-    hourly_value FLOAT;
-    excess_injection FLOAT;
+	market_id INT;
+    cdi_number INT;
 BEGIN
-    -- EA
+    -- calcular cantidad total_consumption
     SELECT SUM(value) INTO total_consumption 
     FROM consumption AS c
 	INNER JOIN records r ON r.id_record = c.id_record
     WHERE r.id_service = service_id;
 
-	-- EC
+  	-- calcular cantidad total_injection
    	SELECT SUM(value) INTO total_injection 
     FROM injection AS i
 	INNER JOIN records r ON r.id_record = i.id_record
     WHERE r.id_service = service_id;
 
-	EA = total_consumption;
-	EC = total_injection;
+  	SELECT s.voltage_level, s.id_market, s.cdi 
+		INTO voltage_level_number, market_id, cdi_number 
+    FROM services s
+    WHERE s.id_service = service_id;
+
+	IF voltage_level_number = 2 OR voltage_level_number = 3 
+		THEN
+			SELECT CU INTO tariff_CU
+			FROM tariffs t
+			WHERE t.id_market = market_id
+			AND t.voltage_level IN (2, 3)
+			LIMIT 1;
+		ELSE
+			SELECT CU INTO tariff_CU
+	        FROM tariffs t
+	        WHERE t.id_market = market_id
+	        AND t.cdi = cdi_number
+	        AND t.voltage_level = voltage_level_number;
+	END IF;
+
+	SELECT C 
+		INTO tariff_C
+    FROM tariffs
+    WHERE id_market = market_id
+    AND voltage_level = voltage_level_number;
+
+	EC := total_injection * tariff_C;
+	EA := total_consumption * tariff_CU;
+
+	-- EE1
+	IF total_injection <= total_consumption
+		THEN
+			EE1 := total_injection * (-tariff_CU);
+		ELSE
+			EE1 := total_consumption * (-tariff_CU);
+	END IF;
+
+	-- EE2
+	IF total_injection <= total_consumption
+		THEN 
+			EE2 := 0;
+		ELSE
+			SELECT SUM((c.value - i.value) * b.value)
+			INTO tariff_EE2
+			FROM consumption c
+			INNER JOIN injection i ON i.id_record = c.id_record
+			INNER JOIN records r ON r.id_record = c.id_record
+			INNER JOIN xm_data_hourly_per_agent b ON b.record_timestamp = r.record_timestamp
+			WHERE r.id_service = service_id;
+	
+	        EE2 := tariff_EE2;
+	END IF;
 
     RETURN;
 END;
